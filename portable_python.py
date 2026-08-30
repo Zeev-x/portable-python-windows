ce9270828bf5c67efbf59abbe8197276520a15d4
Reyette Roxylious Atelier <87901027+Zeev-x@users.noreply.github.com>
2026-08-30 18:32:20 +0700

    Add files via upload

---
+++ portable_python.py
@@ -0,0 +1,115 @@
+import os
+
+path_name = "reyette_py"
+
+main_exe_url = "https://raw.githubusercontent.com/Zeev-x/portable-python-windows/refs/heads/main/pyr.exe"
+
+url_64bit_312 = "https://www.python.org/ftp/python/3.12.10/python-3.12.10-embed-amd64.zip"
+url_32bit_312 = "https://www.python.org/ftp/python/3.12.10/python-3.12.10-embed-win32.zip"
+
+
+url_64bit_314 = "https://www.python.org/ftp/python/3.14.7/python-3.14.7-embed-amd64.zip"
+url_32bit_314 = "https://www.python.org/ftp/python/3.14.7/python-3.14.7-embed-win32.zip"
+
+url_64bit_315 = "https://www.python.org/ftp/python/3.15.0/python-3.15.0a1-embed-amd64.zip"
+url_32bit_315 = "https://www.python.org/ftp/python/3.15.0/python-3.15.0a1-embed-win32.zip"
+
+
+python_12_pth = """python312.zip
+.
+Lib\\site-packages
+..
+
+# Uncomment to run site.main() automatically
+#import site
+"""
+
+python_14_pth = """python314.zip
+.
+Lib\\site-packages
+..
+
+# Uncomment to run site.main() automatically
+#import site
+"""
+
+python_15_pth = """python315.zip
+.
+Lib\\site-packages
+..
+
+# Uncomment to run site.main() automatically
+#import site
+"""
+
+def get_python_version():
+    global py_version
+    while True:
+        vers = input("Select your version (12/14/15): ")
+        if vers in ["12", "14", "15"]:
+            py_version = int(vers)
+            return
+        else:
+            print("Invalid input. Please enter '12', '14' or '15'.")
+
+def get_python_embed_url():
+    while True:
+        arch = input("Enter the architecture (32 or 64): ")
+        if arch == "32":
+            if py_version == 12:
+                return url_32bit_312
+            elif py_version == 14:
+                return url_32bit_314
+            elif py_version == 15:
+                return url_32bit_315
+        elif arch == "64":
+            if py_version == 12:
+                return url_64bit_312
+            elif py_version == 14:
+                return url_64bit_314
+            elif py_version == 15:
+                return url_64bit_315
+        else:
+            print("Invalid input. Please enter '32' or '64'.")
+
+def installer():
+    get_python_version()
+    os.makedirs(path_name, exist_ok=True)
+    cmds = [
+        f"curl -L --output pyr.exe {main_exe_url}",
+        f"curl -L -o python.zip {get_python_embed_url()}",
+        f"powershell -command \"Expand-Archive -Path python.zip -DestinationPath {path_name}\"",
+        f"curl -L -o {path_name}\\get-pip.py https://bootstrap.pypa.io/get-pip.py",
+        f"{path_name}\\python.exe {path_name}\\get-pip.py",
+    ]
+    try:
+        for cmd in cmds:
+            print(f"Executing: {cmd}")
+            os.system(cmd)
+
+        def get_pth_version():
+            if py_version == 12:
+                return "python312._pth", python_12_pth
+            elif py_version == 14:
+                return "python314._pth", python_14_pth
+            elif py_version == 15:
+                return "python315._pth", python_15_pth
+
+        pth1, pth2 = get_pth_version()
+        
+        with open(f"{path_name}\\{pth1}", "w") as f:
+            f.write(pth2)
+
+        return True
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return False
+
+def worker():
+    if installer():
+        os.remove("python.zip")
+        os.remove(f"{path_name}\\get-pip.py")
+        print("Installation completed successfully.")
+
+if __name__ == "__main__":
+    worker()
---
+++ pyr.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import subprocess
+
+def main():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    python_path = os.path.join(script_dir, "reyette_py", "python.exe")
+    #print("Python portable di:", python_path)
+
+    if os.path.exists(python_path):
+        cmd = [python_path] + sys.argv[1:]
+        subprocess.run(cmd, cwd=os.getcwd())
+    else:
+        print(f'Python portable tidak ditemukan di "{python_path}"')
+
+if __name__ == "__main__":
+    main()
